ea3dd5cbe84380983714ec80ff4a6b0f34110e75
Ray-Mhlongo <Ray-Mhlongo@users.noreply.github.com>
2026-06-27 18:01:55 +0200

    Create TV registration signage

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const qrImage = document.getElementById('qrImage');
-const qrMissing = document.getElementById('qrMissing');
 
-qrImage.addEventListener('error', function () {
-  qrImage.style.display = 'none';
-  qrMissing.style.display = 'flex';
-});
-
-qrImage.addEventListener('load', function () {
-  qrImage.style.display = 'block';
-  qrMissing.style.display = 'none';
-});
